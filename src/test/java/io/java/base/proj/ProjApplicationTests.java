997eed5af2998c4d3e08019489a338dce6adc846
kimyk0120 <kimyk0120@hotmail.com>
2023-10-01 14:05:19 +0900

    Initial commit

---
+++ src/test/java/io/java/base/proj/ProjApplicationTests.java
@@ -0,0 +1,13 @@
+package io.java.base.proj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
